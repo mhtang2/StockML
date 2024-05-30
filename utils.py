b0cd9b873e46a1c662becc5a5f43f5fc5e8c2aef
Matthew <mtang317@gmail.com>
2024-05-30 15:17:44 -0500

    wsl updates

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import heapq
+import operator
 
 
 def df_filter_times(df, start_time='9:30', end_time='16:00'):
@@ -9,3 +11,54 @@ def df_filter_times(df, start_time='9:30', end_time='16:00'):
         (df['datetime'].dt.time <= end_time)
     ]
     return filtered_df
+
+
+class Position():
+    def __init__(self, buyprice, buytime, sellby, minprice, fillprice=None, filltype=None):
+        self.buyprice = buyprice
+        self.buytime = buytime
+        self.sellby = sellby
+        self.minprice = minprice
+        self.fillprice = fillprice
+        self.filltype = filltype
+
+    def __lt__(self, obj):
+        return self.buytime < obj.buytime
+
+    def __repr__(self):
+        return f"Buy {self.buyprice}, Minsell {self.minprice}, Fill {self.fillprice} Buytime {self.buytime} Sellby {self.sellby}"
+
+
+class Heap:
+    def __init__(self, usekey=False):
+        self.usekey = usekey
+        self.heap = []
+
+    def push(self, item, key=-1):
+        if self.usekey:
+            heapq.heappush(self.heap, (key, item))
+        else:
+            heapq.heappush(self.heap, item)
+
+    def pop(self):
+        if self.heap:
+            x = heapq.heappop(self.heap)
+            if self.usekey:
+                return x[1]
+            else:
+                return x
+        else:
+            return None
+
+    def peek(self):
+        if self.heap:
+            x = self.heap[0]
+            if self.usekey:
+                return x[1]
+            else:
+                return x
+        else:
+            return None
+
+    def size(self):
+        return len(self.heap)
