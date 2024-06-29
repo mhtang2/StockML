825893c6ed279d6b1a21be904fd790c6ef115f49
Matthew T <mtang317@gmail.com>
2024-06-30 00:08:27 +0800

    local

--- utils.py
+++ utils.py
@@ -1,15 +1,64 @@
 import pandas as pd
 import heapq
 import operator
+import plotly.express as px
+import plotly.graph_objects as go
 
 
-def df_filter_times(df, start_time='9:30', end_time='16:00'):
+_ohlc = ['open','high','low','close']
+_ohlcv = _ohlc + ['volume']
+
+
+def printlist(l, title=""):
+    print(title)
+    for x in l:
+        print(x)
+    
+
+def filter_name_date(df, name, date=None):
+    # filter names
+    df = df.groupby("name").get_group(name).copy()
+
+    # print available dates
+    df['date'] = df['datetime'].dt.date
+    available_dates = df['date'].unique()
+    print("Available dates")
+    for x in available_dates:
+        print(str(x))
+
+    # filter by date
+    if date is not None:
+        date = pd.to_datetime(date).date()
+        if date not in available_dates:
+            raise(Exception(f"{date} Date not in data"))
+
+        df = df[df['date'] == date]
+    return df
+
+
+def showOHLC(df):
+    fig = go.Figure(data=go.Candlestick(
+                        x=df['datetime'],
+                        open=df['open'],
+                        high=df['high'],
+                        low=df['low'],
+                        close=df['close']))
+    fig.update_yaxes(fixedrange=False)
+    # fig.show()
+    return fig
+
+
+
+
+
+def df_filter_times(df, start_time='9:30', end_time='15:59'):
     start_time = pd.to_datetime(start_time).time()
     end_time = pd.to_datetime(end_time).time()
     filtered_df = df[
         (df['datetime'].dt.time >= start_time) &
         (df['datetime'].dt.time <= end_time)
     ]
+    filtered_df['date'] = filtered_df['datetime'].dt.date
     return filtered_df
 
 
@@ -62,3 +111,16 @@ class Heap:
 
     def size(self):
         return len(self.heap)
+
+
+def get_company_summary(df):
+    def proc(x):
+        return pd.DataFrame({
+            "price_avg":[int(x['close'].mean())],
+            "volume_avg":[int(x['volume'].mean())],
+            "price_med":[int(x['close'].median())],
+            "volume_med":[int(x['volume'].median())],
+
+            })
+    df = df.groupby("name").apply(proc)
+    return df
